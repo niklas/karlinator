5f053c578ba450c3c6507519ce00fb9cb1dac7e4
Niklas Hofer <niklas+dev@lanpartei.de>
2014-02-15 00:36:15 +0100

    handle errors while reading text

--- actions/texts.rb
+++ actions/texts.rb
@@ -5,9 +5,13 @@ require 'ruby-progressbar'
 
 Actions.progressbar "Extrahiere Text", @files.count do |progress|
   @texts = @files.map do |file|
-    yomu = Yomu.new file
-    yomu.text.tap do
-      progress.increment
+    begin
+      progress.title = "%23s" % File.basename(file)
+      yomu = Yomu.new file
+      yomu.text.tap do
+        progress.increment
+      end
+    rescue
     end
-  end
+  end.flatten.reject(&:nil?).reject(&:empty?)
 end
