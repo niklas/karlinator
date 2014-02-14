68a1ba56d170b207280da21e6abccf9b45347461
Niklas Hofer <niklas+dev@lanpartei.de>
2014-02-15 00:35:16 +0100

    process all files in given directory recursivly

--- actions/files.rb
+++ actions/files.rb
@@ -1,3 +1,30 @@
-@files = ARGV
+require 'find'
 
+files = []
+
+ARGV.each do |arg|
+  if File.file?(arg)
+    files << arg
+  elsif FileTest.directory?(arg)
+    Find.find(arg) do |path|
+      name = File.basename(path)
+      if name[0] == ?.
+        Find.prune
+      else
+        if FileTest.directory?(path)
+          next
+        else
+          if name =~ /(pdf|doc|docx)\Z/i
+            files << path
+          end
+        end
+      end
+    end
+  end
+end
+
+
+@files = files
 puts "#{@files.count} Dateien"
+
+# find /media/niklas/Spinn/Archive/karl_hecht/Werke | ruby -n -e 'puts $1 if $_ =~ /\.([^.]+)$/'  | sort | uniq
